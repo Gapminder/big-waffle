c9f042c6f76839cf0c475a9cd8b9c50309f47699
Robert Aarts <robert@lokkilok.com>
2019-03-06 12:52:17 +0200

    More service tests.

--- test/service.js
+++ test/service.js
@@ -1,5 +1,6 @@
 const { execFileSync } = require('child_process')
 
+const moment = require('moment')
 const { after, before, describe, it } = require('mocha')
 const request = require('supertest')
 const chai = require('chai')
@@ -19,20 +20,20 @@ const cliOptions = {
   timeout: cliTimeout
 }
 
-function loadTestData (name = 'test', asVersion) {
-  const args = ['src/cli.js', 'load', '-d', 'test/ddf--testdata', 'test']
-  if (asVersion) {
-    args.push(asVersion)
+function loadTestData (name = 'test', version) {
+  const args = ['src/cli.js', 'load', '-d', `test/ddf--testdata${version ? `.${version}` : ''}`, 'test']
+  if (version) {
+    args.push(version)
   }
   return execFileSync('node', args, cliOptions)
 }
 
-function DDFQueryClient (dataset = 'test', version) {
+function DDFQueryClient (dataset = 'test') {
   const client = request(`http://localhost:${HTTPPort}`)
-  client.query = function (ddfQueryObject, useUrlon = false) {
+  client.query = function (ddfQueryObject, version, useUrlon = false) {
     const versionPart = version ? `/${version}` : ''
     const queryString = useUrlon ? encodeURIComponent(Urlon.stringify(ddfQueryObject)) : encodeURIComponent(JSON.stringify(ddfQueryObject))
-    return client.get(`/${dataset}${versionPart}?${queryString}`)
+    return client.get(`/${dataset}${versionPart}?${queryString}`).redirects(version ? 0 : 1)
   }
   return client
 }
@@ -40,24 +41,34 @@ function DDFQueryClient (dataset = 'test', version) {
 describe('DDF Service', function () {
   this.timeout(cliTimeout) // loading test data takes a few seconds
 
-  let client, service
+  let client, service, todaysVersion
 
   before('Load test data and start service', function () {
-    loadTestData()
+    // first make sure to delete all test datasets
+    execFileSync('node', ['src/cli.js', 'delete', 'test', '_ALL_'], cliOptions)
+    // load test data
+    loadTestData('test') // this will have a version based on the date, e.g. "2019030601"
+    loadTestData('test', 'v1') // this will have version 'v1'
+    todaysVersion = `${moment.utc().format('YYYYMMDD')}01`
+    // start the service
     service = DDFService(true) // "forTesting = true" to avoid throttling and spurious logging.
+    // prepare the client
     client = DDFQueryClient()
   })
 
   after('Stop service and delete test data', function (done) {
-    execFileSync('node', ['src/cli.js', 'delete', 'test', '_ALL_'], cliOptions)
-    DB.end()
-      .then(() => {
-        if (service) {
-          service.close(done)
-        } else {
-          done()
-        }
-      })
+    function cleanUp () {
+      DB.end()
+        .then(function () {
+          execFileSync('node', ['src/cli.js', 'delete', 'test', '_ALL_'], cliOptions)
+        })
+        .then(done)
+    }
+    if (service) {
+      service.close(cleanUp)
+    } else {
+      cleanUp()
+    }
   })
 
   describe('List endpoint', function () {
@@ -66,7 +77,146 @@ describe('DDF Service', function () {
         .set('Accept', 'application/json')
         .expect(200)
         .then(response => {
-          response.body.should.be.an('array').that.contains.something.like({ name: 'test' })
+          const list = response.body
+          list.should.be.an('array').that.contains.something.eql({ name: 'test', version: 'v1' })
+          list.should.contain.something.eql({ name: 'test', version: todaysVersion })
+        })
+    })
+    it('no default version', function () {
+      return client.get('/')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          const list = response.body
+          list.should.contain.not.something.like({ name: 'test', default: true })
+        })
+    })
+  })
+
+  describe('Query endpoint', function () {
+    it('most recent used as default', function () {
+      return client.query({
+        select: { key: ['key', 'value'], value: [] },
+        from: 'concepts.schema'
+      })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          response.body.should.be.like({ version: 'v1' })
+        })
+    })
+    it('set default version', function () {
+      execFileSync('node', ['src/cli.js', 'make-default', 'test', todaysVersion], cliOptions)
+      return client.query({
+        select: { key: ['key', 'value'], value: [] },
+        from: 'concepts.schema'
+      })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          response.body.should.be.like({ version: todaysVersion })
+        })
+    })
+    it('invalid query: key', function () {
+      return client.query({
+        select: { key: 'key value', value: [] },
+        from: 'concepts.schema'
+      })
+        .set('Accept', 'application/json')
+        .expect(400)
+    })
+    it('invalid query: value', function () {
+      return client.query({
+        select: { key: ['key', 'value'], values: [] },
+        from: 'concepts.schema'
+      })
+        .set('Accept', 'application/json')
+        .expect(400)
+    })
+    it('invalid query: from missing', function () {
+      return client.query({
+        select: { key: ['key', 'value'], value: [] }
+      })
+        .set('Accept', 'application/json')
+        .expect(400)
+    })
+    it('concepts schema', function () {
+      return client.query({
+        select: { key: ['key', 'value'], value: [] },
+        from: 'concepts.schema'
+      })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          response.body.should.be.an('object')
+          response.body.should.have.keys(['header', 'rows', 'version'])
+          response.body.header.should.have.members(['key', 'value'])
+          response.body.rows.should.have.lengthOf(5)
+          response.body.rows.should.contain.one.deep.equal([['concept'], 'concept_type'])
+        })
+    })
+    it('fetch entities with domains', function () {
+      return client.query({
+        select: { key: ['concept'], value: ['domain'] },
+        from: 'concepts',
+        where: {
+          concept_type: 'entity_set'
+        }
+      })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          response.body.should.be.an('object')
+          response.body.should.have.keys(['header', 'rows', 'version'])
+          response.body.header.should.have.members(['concept', 'domain'])
+          response.body.rows.should.contain.one.eql(['city', 'geo'])
+          response.body.rows.should.contain.one.eql(['gas', null])
+        })
+    })
+    it('fetch populations in cities', function () {
+      return client.query({
+        select: { key: ['city', 'time'], value: ['population'] },
+        from: 'datapoints',
+        where: {
+          time: { $gte: 2000, $lte: 2015 }
+        },
+        order_by: ['population']
+      })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          response.body.should.be.an('object')
+          response.body.should.have.keys(['header', 'rows', 'version'])
+          response.body.header.should.have.members(['city', 'time', 'population'])
+          response.body.rows.should.have.lengthOf(2)
+          response.body.rows.should.contain.one.eql(['male', 2000, 34567])
+          response.body.rows.should.contain.one.eql(['mariehamn_ala', 2000, 12345])
+        })
+    })
+    it('fetch populations in southern cities', function () {
+      return client.query({
+        select: { key: ['city', 'gender', 'time'], value: ['population'] },
+        from: 'datapoints',
+        where: {
+          $and: [{ geo: '$geo' }]
+        },
+        join: {
+          $geo: {
+            key: 'geo',
+            where: { latitude: { $lt: 25 } }
+          }
+        },
+        order_by: ['population']
+      })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          response.body.should.be.an('object')
+          response.body.should.have.keys(['header', 'rows', 'version'])
+          response.body.header.should.have.members(['city', 'gender', 'time', 'population'])
+          response.body.rows.should.have.lengthOf(4)
+          response.body.rows.should.contain.one.eql(['male', 'male', 1991, 12346])
+          response.body.rows.should.contain.one.eql(['hongkong', 'female', 1991, 567890])
         })
     })
   })
