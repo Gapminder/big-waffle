42393fc283df8981952fba5e7ac5df6af07d11eb
Robert Aarts <robert@lokkilok.com>
2019-12-24 09:36:17 +0200

    Removing record limiters from loggers, to check...

--- src/log-google.js
+++ src/log-google.js
@@ -1,40 +1,9 @@
-const { Transform } = require('stream')
 const { LoggingBunyan } = require('@google-cloud/logging-bunyan')
 
 const GoogleLogLevel = process.env.GOOGLE_LOG_LEVEL || process.env.LOG_LEVEL || 'info'
 const GoogleLogName = process.env.GOOGLE_LOG_NAME || 'master'
 
-const RecordLimit = 50000
-const ShorterMessageLength = 10000
-
 module.exports = (logger) => {
-  const logRecordLimiter = new Transform({
-    objectMode: true,
-
-    transform (obj, encoding, callback) {
-      const str = JSON.stringify(obj)
-      if (str.length > RecordLimit) {
-        logger.warn('Received a too long log record!')
-        const smallerObj = {
-          hostname: obj.hostname,
-          level: obj.level,
-          msg: obj.msg.slice(0, ShorterMessageLength),
-          name: obj.name,
-          pid: obj.pid,
-          time: obj.time,
-          v: obj.v
-        }
-        if (obj.module) {
-          smallerObj.module = obj.module
-        }
-        this.push(smallerObj)
-        callback()
-      } else {
-        this.push(obj)
-        callback()
-      }
-    }
-  })
   const googleLog = new LoggingBunyan({
     logName: GoogleLogName,
     projectId: 'big-waffle',
@@ -43,6 +12,6 @@ module.exports = (logger) => {
       version: 1
     }
   })
-  logRecordLimiter.pipe(googleLog.stream(GoogleLogLevel).stream, { end: false })
-  logger.addStream({ level: GoogleLogLevel, type: 'raw', stream: logRecordLimiter, reemitErrorEvents: false })
+
+  logger.addStream(googleLog.stream(GoogleLogLevel))
 }
